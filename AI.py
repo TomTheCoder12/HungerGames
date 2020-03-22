d5cdbfde1375674af30a15a2861bae30f56513f7
TomTheCoder12 <46482830+TomTheCoder12@users.noreply.github.com>
2020-03-22 11:03:00 +0100

    Add files via upload

--- AI.py
+++ AI.py
@@ -1,4 +1,61 @@
 from matrix import matrix
 import random
+
+t = matrix(3,3,custom=[[" ","O","^"],[" ","O","^"],[" ","O","^"]])
+tc = matrix(3,3,custom=[[" ","O","^"],[" ","O","^"],[" ","O","^"]])
+
 class ai:
-    pass
+    def __init__(self,terrain,constant,startpos,name,stampchar="auto"):
+        global land
+        land = terrain
+        self.constant = constant
+        self.name = name
+        self.state = "idle"
+        self.pos = startpos
+        if stampchar == "auto":
+            land.set_char(self.pos[0],self.pos[1],name[0])
+            self.char = name[0]
+        elif stampchar == False:
+            land.set_char(self.pos[0],self.pos[1],stampchar)
+            self.char = stampchar
+#        else:
+#            land.set_char(self.pos[0],self.pos[1],name[0])
+    def move(self,direction):
+        land.set_char(self.pos[0],self.pos[1],self.constant.get_char(self.pos[0],self.pos[1]))
+        if direction == 0:
+            movelist = [(self.pos[0])+1,self.pos[1]]
+            self.pos = [(self.pos[0])+1,self.pos[1]]
+        elif direction == 1:
+            movelist = [(self.pos[0])-1,self.pos[1]]
+            self.pos = [(self.pos[0])-1,self.pos[1]]
+        elif direction == 2:
+            movelist = [(self.pos[0]),(self.pos[1])+1]
+            self.pos = [(self.pos[0]),(self.pos[1])+1]
+        elif direction == 3:
+            movelist = [(self.pos[0]),(self.pos[1])-1]
+            self.pos = [(self.pos[0]),(self.pos[1])-1]
+        land.set_char(self.pos[0],self.pos[1],self.char)
+    def coords(self):
+        return self.pos
+
+jeff = ai(t,tc,[0,0],"jeff")
+        
+"""
+    def move(self,direction,stamp=True):
+        if direction == 0:
+            movelist = [(self.pos[0])+1,self.pos[1]]
+            land.move_char(self.pos,movelist,auto=self.constant.get_char(self.pos[0],self.pos[1]))
+            self.pos = [(self.pos[0])+1,self.pos[1]]
+        elif direction == 1:
+            movelist = [(self.pos[0])-1,self.pos[1]]
+            land.move_char(self.pos,movelist,auto=self.constant.get_char(self.pos[0],self.pos[1]))
+            self.pos = [(self.pos[0])-1,self.pos[1]]
+        elif direction == 2:
+            movelist = [(self.pos[0]),(self.pos[1])+1]
+            land.move_char(self.pos,movelist,auto=self.constant.get_char(self.pos[0],self.pos[1]))
+            self.pos = [(self.pos[0]),(self.pos[1])+1]
+        elif direction == 3:
+            movelist = [(self.pos[0]),(self.pos[1])-1]
+            land.move_char(self.pos,movelist,auto=self.constant.get_char(self.pos[0],self.pos[1]))
+            self.pos = [(self.pos[0]),(self.pos[1])-1]
+"""
