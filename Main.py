8c34b849c4e1553ba1b6b74445b495e284760410
TomTheCoder12 <46482830+TomTheCoder12@users.noreply.github.com>
2020-04-02 14:49:07 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 from matrix import matrix
+from entity import entity
 import random
+import time
 
 
-"""
+
 rmgdone = 0
 while rmgdone == 0:
     try:
@@ -15,17 +17,13 @@ while rmgdone == 0:
         p2 = int(input("How much coniferous forest>>> "))
         p3 = int(input("How many mountains>>> "))
         p4 = int(input("How much water >>> "))
-        print((x*y)-(p1+p2+p3+p4),"empty spaces")
+        print((x*y)-(p1+p2+p3+p4)," plains biomes.")
         biomes.append(" "*((x*y)-(p1+p2+p3+p4)))
        
         def randgenlist(place,char):
             indbiomes = []
             for i in range(0,place):
                 indbiomes.append(char)
-            #string = ""
-            #indbiomes = string.join(indbiomes)
-            #indbiomes = list(indbiomes)
-            #print(indbiomes)
             return indbiomes
         biomes.append(randgenlist(p1,"^"))
         biomes.append(randgenlist(p2,"*"))
@@ -54,24 +52,35 @@ while rmgdone == 0:
             temp_2.append(temp_1)
 
         terrain = temp_2
-        print("Terrain generated=",terrain)
+        print("Terrain generated =")
         t = matrix((len(terrain)),(len(terrain[0])),custom=terrain)
+        t.draw_grid()
+        time.sleep(1)
+        print("^ = Pine forest")
+        print("* = Coniferous forest")
+        print("O = Mountain")
+        print("~ = Water")
         rmgdone = 1
     except:
         print("Invalid generation")
 
-names = ["Fred","Frodo","Sir Tom Higgithenobotham","Tom Arto","Jake Sell","Dungham MacFred","Donaldus Troomp","Tom Dickenharry","A random guy"]
+print("Preparing objects")
+
+#Crossbow, trap, sword, dagger, axe
+
+names = ["Fred","Frodo","Sir Tom Higgithenobotham","Tom Arto","Jake Sell","Dungham MacFred","Donaldus Troomp","Tom Dickenharry","William Wallace Son of Scotia","The Irish Potato Blight","The queen of England","Impy","Art","D-Bug","Ele-D","Jeff"]
 
 people = []
 for x in range(0,3):
-    rand = random.randint(0,len(names))
+    rand = random.randint(0,len(names)-1)
     people.append(names[rand])
     names.remove(names[rand])
 print("The combatants are: ")
 for x in range(0,len(people)):
     print(people[x])
 
+#Game loop
 while not input("Press enter for next timestep (type 0 to quit) >>> ") == "0":
     t.draw_grid()
-
-"""
+    
+    
